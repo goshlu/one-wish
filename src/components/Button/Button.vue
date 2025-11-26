5f9f5829090cbf11eb667e3b86cd07e1e19d822b
wishplash <yangshiheclow@gmail.com>
2025-11-26 16:39:25 +0800

    feat(Button): 替换加载动画为SVG图标
    将按钮组件中的加载动画从⏳表情符号替换为更专业的SVG旋转图标，提升视觉一致性

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -5,50 +5,58 @@
     :disabled="disabled || loading"
     @click="handleClick"
   >
-    <span v-if="loading" class="my-button__loading">⏳</span>
+    <span v-if="loading" class="my-button__loading"
+      >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+        <path
+          fill="currentColor"
+          d="M512 64a32 32 0 0 1 32 32v192a32 32 0 0 1-64 0V96a32 32 0 0 1 32-32m0 640a32 32 0 0 1 32 32v192a32 32 0 1 1-64 0V736a32 32 0 0 1 32-32m448-192a32 32 0 0 1-32 32H736a32 32 0 1 1 0-64h192a32 32 0 0 1 32 32m-640 0a32 32 0 0 1-32 32H96a32 32 0 0 1 0-64h192a32 32 0 0 1 32 32M195.2 195.2a32 32 0 0 1 45.248 0L376.32 331.008a32 32 0 0 1-45.248 45.248L195.2 240.448a32 32 0 0 1 0-45.248m452.544 452.544a32 32 0 0 1 45.248 0L828.8 783.552a32 32 0 0 1-45.248 45.248L647.744 692.992a32 32 0 0 1 0-45.248M828.8 195.264a32 32 0 0 1 0 45.184L692.992 376.32a32 32 0 0 1-45.248-45.248l135.808-135.808a32 32 0 0 1 45.248 0m-452.544 452.48a32 32 0 0 1 0 45.248L240.448 828.8a32 32 0 0 1-45.248-45.248l135.808-135.808a32 32 0 0 1 45.248 0"
+        ></path></svg
+    >
+    </span>
     <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import type { ButtonProps, ButtonEmits } from './types'
+import { computed } from "vue";
+import type { ButtonProps, ButtonEmits } from "./types";
 
 defineOptions({
-  name: 'MyButton',
-})
+  name: "MyButton",
+});
 
 const props = withDefaults(defineProps<ButtonProps>(), {
-  type: 'default',
-  size: 'default',
-  nativeType: 'button',
+  type: "default",
+  size: "default",
+  nativeType: "button",
   disabled: false,
   loading: false,
   round: false,
   plain: false,
-})
+});
 
-const emit = defineEmits<ButtonEmits>()
+const emit = defineEmits<ButtonEmits>();
 
 const buttonClass = computed(() => {
   return [
-    'my-button',
+    "my-button",
     `my-button--${props.type}`,
     `my-button--${props.size}`,
     {
-      'is-disabled': props.disabled,
-      'is-loading': props.loading,
-      'is-round': props.round,
-      'is-plain': props.plain,
+      "is-disabled": props.disabled,
+      "is-loading": props.loading,
+      "is-round": props.round,
+      "is-plain": props.plain,
     },
-  ]
-})
+  ];
+});
 
 const handleClick = (event: MouseEvent) => {
   if (!props.disabled && !props.loading) {
-    emit('click', event)
+    emit("click", event);
   }
-}
+};
 </script>
 
 <style scoped>
@@ -189,6 +197,12 @@ const handleClick = (event: MouseEvent) => {
 .my-button__loading {
   display: inline-block;
   animation: rotate 1s linear infinite;
+}
+
+.my-button__loading svg {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
 }
 
 @keyframes rotate {
